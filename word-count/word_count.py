3f708334a58757d54d1d5241db9a57069c3b8fc8
YasharHND <yashar@sandboxapp.io>
2022-10-25 15:33:19 +0300

    word count

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,2 +1,22 @@
+def add_or_increment(words, word):
+    while word[-1] == "'":
+        word = word[:-1]
+    word = word.lower()
+    if word in words:
+        words[word] += 1
+    else:
+        words[word] = 1
+
+
 def count_words(sentence):
-    pass
+    out = {}
+    word = ""
+    for char in sentence:
+        if char.isalnum() or (char == "'" and len(word) > 0):
+            word += char
+        elif len(word) > 0:
+            add_or_increment(out, word)
+            word = ""
+    if len(word) > 0:
+        add_or_increment(out, word)
+    return out

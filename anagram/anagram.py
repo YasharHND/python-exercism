08350a03f8969470507883a7aad6389919625035
YasharHND <yashar@sandboxapp.io>
2022-11-22 18:06:12 +0300

    Anagram

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,2 +1,16 @@
 def find_anagrams(word, candidates):
-    pass
+    anagrams = []
+    for candidate in candidates:
+        candidate_l = candidate.lower()
+        word_l = word.lower()
+        if candidate_l == word_l:
+            continue
+        for char in candidate_l:
+            if char not in word_l:
+                break
+            index = word_l.index(char)
+            word_l = word_l[0:index] + word_l[index + 1:]
+        else:
+            if len(word_l) == 0:
+                anagrams.append(candidate)
+    return anagrams

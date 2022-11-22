b0ba73f13e68e79aa83c66cf976a8ebe2f16526f
YasharHND <yashar@sandboxapp.io>
2022-11-22 18:51:41 +0300

    Scrabble Score

--- scrabble-score/scrabble_score.py
+++ scrabble-score/scrabble_score.py
@@ -1,2 +1,23 @@
+scores = {
+    1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+    2: ["D", "G"],
+    3: ["B", "C", "M", "P"],
+    4: ["F", "H", "V", "W", "Y"],
+    5: ["K"],
+    8: ["J", "X"],
+    10: ["Q", "Z"]
+}
+
+
+def find_score(char):
+    char = char.upper()
+    for value, characters in scores.items():
+        if char in characters:
+            return value
+
+
 def score(word):
-    pass
+    final_score = 0
+    for character in word:
+        final_score += find_score(character)
+    return final_score

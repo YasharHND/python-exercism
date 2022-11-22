f6505c6c131919bec9815b39794fedcf8da9a3e5
YasharHND <yashar@sandboxapp.io>
2022-11-22 19:11:25 +0300

    Nth Prime

--- nth-prime/nth_prime.py
+++ nth-prime/nth_prime.py
@@ -1,2 +1,25 @@
+import math
+
+
+def is_prime(number):
+    if number == 2:
+        return True
+    if number % 2 == 0:
+        return False
+    for i in range(3, int(math.sqrt(number) + 1)):
+        if number % i == 0:
+            return False
+    return True
+
+
 def prime(number):
-    pass
+    if number < 1:
+        raise ValueError("there is no zeroth prime")
+    prime_count = 1
+    n = 2
+    while True:
+        if prime_count == number:
+            return n
+        n += 1
+        if is_prime(n):
+            prime_count += 1

ab7b4789ea15712592eb0ef4418cd89db04dd183
YasharHND <yashar@sandboxapp.io>
2022-11-22 17:51:21 +0300

    Hamming

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,2 +1,8 @@
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Strands must be of equal length.")
+    hamming_distance = 0
+    for i in range(len(strand_a)):
+        if strand_a[i] != strand_b[i]:
+            hamming_distance += 1
+    return hamming_distance

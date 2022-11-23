12ec4fe25510e5c494e945fbef179b9aa96b65c9
YasharHND <yashar@sandboxapp.io>
2022-11-23 13:36:44 +0300

    Series

--- series/series.py
+++ series/series.py
@@ -1,2 +1,10 @@
 def slices(series, length):
-    pass
+    if length == 0:
+        raise ValueError("slice length cannot be zero")
+    if length < 0:
+        raise ValueError("slice length cannot be negative")
+    if len(series) == 0:
+        raise ValueError("series cannot be empty")
+    if len(series) < length:
+        raise ValueError("slice length cannot be greater than series length")
+    return [series[i:i + length] for i in range((len(series) - length) + 1)]

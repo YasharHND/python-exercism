d038d5b8cbb5ad8268f332989a44c90aeeea503f
YasharHND <yashar@sandboxapp.io>
2022-11-23 15:50:33 +0300

    Prime Factors

--- prime-factors/prime_factors.py
+++ prime-factors/prime_factors.py
@@ -1,2 +1,15 @@
-def factors(value):
-    pass
+import math
+
+
+def factors(n):
+    out = []
+    while n % 2 == 0:
+        out.append(2)
+        n = n / 2
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        while n % i == 0:
+            out.append(i)
+            n = n / i
+    if n > 2:
+        out.append(int(n))
+    return out

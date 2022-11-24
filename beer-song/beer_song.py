ae7b34cc43ef2864a9d70c12d384026074e7b0d8
YasharHND <yashar@sandboxapp.io>
2022-11-24 16:45:14 +0300

    Beer Song

--- beer-song/beer_song.py
+++ beer-song/beer_song.py
@@ -1,2 +1,23 @@
+def bottle(count):
+    if count > 1:
+        return f"{count} bottles"
+    if count == 1:
+        return "1 bottle"
+    return "No more bottles"
+
+
 def recite(start, take=1):
-    pass
+    out = []
+    empty_line = False
+    start += 1
+    for i in reversed(range(start - take, start)):
+        bottles = bottle(i)
+        if empty_line:
+            out.append("")
+        out.append(f"{bottles} of beer on the wall, {bottles.lower()} of beer.")
+        if i > 0:
+            out.append(f"Take {'one' if i > 1 else 'it'} down and pass it around, {bottle(i - 1).lower()} of beer on the wall.")
+        else:
+            out.append("Go to the store and buy some more, 99 bottles of beer on the wall.")
+        empty_line = True
+    return out

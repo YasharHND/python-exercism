8c9399cdb12154efd3ad792de81b3710f0712311
YasharHND <yashar@sandboxapp.io>
2022-11-25 18:51:03 +0300

    Circular Buffer

---
+++ circular-buffer/circular_buffer.py
@@ -0,0 +1,33 @@
+class BufferFullException(BufferError):
+    def __init__(self, message):
+        self.message = message
+
+
+class BufferEmptyException(BufferError):
+    def __init__(self, message):
+        self.message = message
+
+
+class CircularBuffer:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.buffer = []
+
+    def read(self):
+        if len(self.buffer) == 0:
+            raise BufferEmptyException("Circular buffer is empty")
+        return self.buffer.pop(0)
+
+    def write(self, data):
+        if len(self.buffer) == self.capacity:
+            raise BufferFullException("Circular buffer is full")
+        self.buffer.append(data)
+
+    def overwrite(self, data):
+        if len(self.buffer) == self.capacity:
+            self.buffer.pop(0)
+        self.buffer.append(data)
+
+    def clear(self):
+        self.buffer.clear()
+

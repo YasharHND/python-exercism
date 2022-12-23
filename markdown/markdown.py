ef62cceebe1c0e03fe38f6021043c0b29a1aa9c0
YasharHND <yashar@sandboxapp.io>
2022-12-23 15:07:13 +0300

    Markdown

--- markdown/markdown.py
+++ markdown/markdown.py
@@ -1,77 +1,41 @@
 import re
 
 
-def parse(markdown):
-    lines = markdown.split('\n')
-    res = ''
-    in_list = False
-    in_list_append = False
-    for i in lines:
-        if re.match('###### (.*)', i) is not None:
-            i = '<h6>' + i[7:] + '</h6>'
-        elif re.match('##### (.*)', i) is not None:
-            i = '<h5>' + i[6:] + '</h5>'
-        elif re.match('#### (.*)', i) is not None:
-            i = '<h4>' + i[5:] + '</h4>'
-        elif re.match('### (.*)', i) is not None:
-            i = '<h3>' + i[4:] + '</h3>'
-        elif re.match('## (.*)', i) is not None:
-            i = '<h2>' + i[3:] + '</h2>'
-        elif re.match('# (.*)', i) is not None:
-            i = '<h1>' + i[2:] + '</h1>'
-        m = re.match(r'\* (.*)', i)
-        if m:
-            if not in_list:
-                in_list = True
-                is_bold = False
-                is_italic = False
-                curr = m.group(1)
-                m1 = re.match('(.*)__(.*)__(.*)', curr)
-                if m1:
-                    curr = m1.group(1) + '<strong>' + \
-                        m1.group(2) + '</strong>' + m1.group(3)
-                    is_bold = True
-                m1 = re.match('(.*)_(.*)_(.*)', curr)
-                if m1:
-                    curr = m1.group(1) + '<em>' + m1.group(2) + \
-                        '</em>' + m1.group(3)
-                    is_italic = True
-                i = '<ul><li>' + curr + '</li>'
-            else:
-                is_bold = False
-                is_italic = False
-                curr = m.group(1)
-                m1 = re.match('(.*)__(.*)__(.*)', curr)
-                if m1:
-                    is_bold = True
-                m1 = re.match('(.*)_(.*)_(.*)', curr)
-                if m1:
-                    is_italic = True
-                if is_bold:
-                    curr = m1.group(1) + '<strong>' + \
-                        m1.group(2) + '</strong>' + m1.group(3)
-                if is_italic:
-                    curr = m1.group(1) + '<em>' + m1.group(2) + \
-                        '</em>' + m1.group(3)
-                i = '<li>' + curr + '</li>'
-        else:
-            if in_list:
-                in_list_append = True
-                in_list = False
+def replace_styles(line):
+    while True:
+        match = re.search("__(.+)__", line)
+        if not match:
+            break
+        line = line.replace(match.group(0), f"<strong>{match.group(1)}</strong>")
+    while True:
+        match = re.search("_(.+)_", line)
+        if not match:
+            break
+        line = line.replace(match.group(0), f"<em>{match.group(1)}</em>")
+    return line
 
-        m = re.match('<h|<ul|<p|<li', i)
-        if not m:
-            i = '<p>' + i + '</p>'
-        m = re.match('(.*)__(.*)__(.*)', i)
-        if m:
-            i = m.group(1) + '<strong>' + m.group(2) + '</strong>' + m.group(3)
-        m = re.match('(.*)_(.*)_(.*)', i)
-        if m:
-            i = m.group(1) + '<em>' + m.group(2) + '</em>' + m.group(3)
-        if in_list_append:
-            i = '</ul>' + i
-            in_list_append = False
-        res += i
-    if in_list:
-        res += '</ul>'
-    return res
+
+def parse_line(line, last_line_bullet):
+    header_match = re.search("^(#{1,6}) (.+)$", line)
+    if header_match:
+        tag = f"h{header_match.group(1).count('#')}"
+        return f"<{tag}>{replace_styles(header_match.group(2))}</{tag}>", False
+    bullet_match = re.search("^\\* (.+)$", line)
+    if bullet_match:
+        return f"{'' if last_line_bullet else '<ul>'}<li>{replace_styles(bullet_match.group(1))}</li>", True
+    return f"<p>{replace_styles(line)}</p>", False
+
+
+def parse(markdown):
+    out = []
+    last_line_bullet = False
+    for line in markdown.split('\n'):
+        line, this_line_bullet = parse_line(line, last_line_bullet)
+        if last_line_bullet and not this_line_bullet:
+            out[-1] += "</ul>"
+        last_line_bullet = this_line_bullet
+        out.append(line)
+    else:
+        if last_line_bullet:
+            out[-1] += "</ul>"
+    return "".join(out)

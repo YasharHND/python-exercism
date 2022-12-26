5f3325aa0e8bbde73efb1eb69a3b2fc4df455dde
YasharHND <yashar@sandboxapp.io>
2022-12-26 13:48:12 +0300

    Largest Series Product

--- largest-series-product/largest_series_product.py
+++ largest-series-product/largest_series_product.py
@@ -1,2 +1,17 @@
+import functools
+
+
 def largest_product(series, size):
-    pass
+    if len(series) < size:
+        raise ValueError("span must be smaller than string length")
+    if size == 0:
+        return 1
+    if not series.isdigit():
+        raise ValueError("digits input must only contain digits")
+    if size < 0:
+        raise ValueError("span must not be negative")
+    out = 0
+    for i in range((len(series) - size) + 1):
+        product = functools.reduce(lambda x, y: x * y, map(int, [c for c in series[i:i + size]]))
+        out = max(out, product)
+    return out
